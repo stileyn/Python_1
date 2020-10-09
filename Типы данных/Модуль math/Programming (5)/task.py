7b3ca539a1dd98f0b495d5aa7e444c24092eaccc
jzetko <jzetkor@gmail.com>
2020-10-09 06:56:42 +0300

    Квадратное уравнение

--- Типы данных/Модуль math/Programming (5)/task.py
+++ Типы данных/Модуль math/Programming (5)/task.py
@@ -1,1 +1,20 @@
-# put your python code here
+# Даны три вещественных числа a, b, c. Напишите программу, которая находит вещественные корни квадратного уравнения
+# ax2+bx+c=0.
+
+from math import *
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+discr = b ** 2 - 4 * a * c
+
+if discr > 0:
+    x1 = (-b + sqrt(discr)) / (2 * a)
+    x2 = (-b - sqrt(discr)) / (2 * a)
+    print(min(x1, x2), max(x1, x2), sep='\n')
+elif discr == 0:
+    x = -b / (2 * a)
+    print(x)
+else:
+    print("Нет корней")
