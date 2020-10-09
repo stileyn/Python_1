f4367130e3efb263b7a0d6d496b7541005fd4efe
jzetko <jzetkor@gmail.com>
2020-10-09 06:01:15 +0300

    Абсолютная сумма

--- Типы данных/Числовые типы данных  int, float/Programming (10)/task.py
+++ Типы данных/Числовые типы данных  int, float/Programming (10)/task.py
@@ -1,1 +1,5 @@
-# put your python code here
+# Даны пять чисел a1,a2,a3,a4,a5. Напишите программу, которая вычисляет сумму их модулей
+
+a1, a2, a3, a4, a5 = [float(input()) for _ in range(5)]
+
+print(abs(a1) + abs(a2) + abs(a3) + abs(a4) + abs(a5))
