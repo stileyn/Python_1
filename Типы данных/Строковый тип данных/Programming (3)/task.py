3f7d46d3d55e938469706afbd3798ce8edd0a86c
jzetko <jzetkor@gmail.com>
2020-10-09 07:33:58 +0300

    Три города

--- Типы данных/Строковый тип данных/Programming (3)/task.py
+++ Типы данных/Строковый тип данных/Programming (3)/task.py
@@ -1,1 +1,6 @@
-# put your python code here
+# Даны названия трех городов. Напишите программу, которая определяет самое короткое и самое длинное название города.
+
+a, b, c = input(), input(), input()
+x, y, z = len(a), len(b), len(c)
+print(a if x < y and x < z else b if y < x and y < z else c)
+print(a if x > y and x > z else b if y > x and y > z else c)
