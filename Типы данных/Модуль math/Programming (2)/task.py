d8430f2e379db4998b2c0d11d986a80513a7ac2f
jzetko <jzetkor@gmail.com>
2020-10-09 06:41:23 +0300

    Средние значения

--- Типы данных/Модуль math/Programming (2)/task.py
+++ Типы данных/Модуль math/Programming (2)/task.py
@@ -1,1 +1,10 @@
-# put your python code here
+# Программа должна вывести 4 числа – среднее арифметическое, геометрическое, гармоническое и квадратичное.
+
+from math import *
+
+a, b = float(input()), float(input())
+
+print((a + b) / 2)
+print(sqrt(a * b))
+print(2 * a * b / (a + b))
+print(sqrt((a * a + b * b) / 2))
