eb4570fe9bbc492e62ddebf46ad3af00ce9b2776
jzetko <jzetkor@gmail.com>
2020-10-09 07:14:17 +0300

    Kонкатенация

--- Типы данных/Строковый тип данных/Programming/task.py
+++ Типы данных/Строковый тип данных/Programming/task.py
@@ -1,1 +1,5 @@
-# put your python code here
+# Напишите программу, которая выводит текст:
+#
+# "Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+
+print('''"Python is a great language!", said Fred. "I don't ever remember having this much fun before."''')
