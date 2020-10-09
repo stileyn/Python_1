0128a80d42afa70858703c8089242a8f7adba0eb
jzetko <jzetkor@gmail.com>
2020-10-09 07:20:30 +0300

    What's Your Name?

--- Типы данных/Строковый тип данных/Programming (1)/task.py
+++ Типы данных/Строковый тип данных/Programming (1)/task.py
@@ -1,1 +1,7 @@
-# put your python code here
+# Напишите программу, которая считывает с клавиатуры две строки – имя и фамилию пользователя и выводит фразу:
+#
+# «Hello [введенное имя] [введенная фамилия]! You just delved into Python».
+
+firstname, lastname = input(), input()
+
+print(f"Hello {firstname} {lastname}! You just delved into Python")
