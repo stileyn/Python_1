9efa989736955c336fbc7147a33961797f4823e2
jzetko <jzetkor@gmail.com>
2020-10-09 06:43:39 +0300

    Тригонометрическое выражение

--- Типы данных/Модуль math/Programming (3)/task.py
+++ Типы данных/Модуль math/Programming (3)/task.py
@@ -1,1 +1,7 @@
-# put your python code here
+# Программа должна вывести одно число – значение тригонометрического выражения.
+
+from math import *
+
+x = float(input())
+
+print(sin(radians(x)) + cos(radians(x)) + tan(radians(x)) ** 2)
