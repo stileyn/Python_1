304c2e80c1b875350ab6ac15f5196b9c6e234f0a
jzetko <jzetkor@gmail.com>
2020-10-09 05:48:59 +0300

    Сортировка трёх

--- Типы данных/Числовые типы данных  int, float/Programming (8)/task.py
+++ Типы данных/Числовые типы данных  int, float/Programming (8)/task.py
@@ -1,1 +1,9 @@
-# put your python code here
+# Напишите программу, которая упорядочивает три числа от большего к меньшему.
+
+number1, number2, number3 = [int(input()) for _ in range(3)]
+
+print(max(number1, number2, number3),
+      number1 + number2 + number3 - max(number1, number2, number3) - min(number1, number2, number3),
+      min(number1, number2, number3), sep='\n')
+
+
