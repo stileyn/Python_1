2d4eed98a1dbd6a5b674f4ae48f9e990cd8bc42c
jzetko <jzetkor@gmail.com>
2020-10-09 05:03:34 +0300

    Dog age

--- Типы данных/Числовые типы данных  int, float/Programming (4)/task.py
+++ Типы данных/Числовые типы данных  int, float/Programming (4)/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+# На вход программе подается число n – количество собачьих лет. Напишите программу,
+# которая вычисляет возраст собаки в человеческих годах.
+
+dog_age = int(input())
+human_age = 0
+
+for i in range(1, dog_age + 1):
+    if i == 1 or i == 2:
+        human_age += 10.5
+    else:
+        human_age += 4
+
+print(human_age)
