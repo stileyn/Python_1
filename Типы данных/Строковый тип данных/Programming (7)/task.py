99307d8407cf06a6e0871035746f99dea1b94bdf
jzetko <jzetkor@gmail.com>
2020-10-10 22:34:00 +0300

    Корректный email

--- Типы данных/Строковый тип данных/Programming (7)/task.py
+++ Типы данных/Строковый тип данных/Programming (7)/task.py
@@ -1,1 +1,9 @@
-# put your python code here
+# Будем считать email адрес корректным, если в нем есть символ собачки (@) и точки.
+# Напишите программу проверяющую корректность email адреса.
+
+s = input()
+
+if '@' in s and '.' in s:
+    print('YES')
+else:
+    print('NO')
