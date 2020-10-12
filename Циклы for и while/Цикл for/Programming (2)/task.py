59143e502557b99338d82a53149fe6ee762e2647
jzetko <jzetkor@gmail.com>
2020-10-12 20:20:30 +0300

    Последовательность символов

--- Циклы for и while/Цикл for/Programming (2)/task.py
+++ Циклы for и while/Цикл for/Programming (2)/task.py
@@ -1,1 +1,14 @@
-# put your python code here
+# Напишите программу, которая использует ровно три цикла for для печати следующей последовательности символов:
+
+for _ in range(6):
+    print('AAA')
+
+for _ in range(5):
+    print('BBBB')
+
+print('E')
+
+for _ in range(9):
+    print('TTTTT')
+
+print('G')
