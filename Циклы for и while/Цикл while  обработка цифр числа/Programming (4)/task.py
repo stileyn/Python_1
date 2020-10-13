a31476e7fca5bf5de4ae2344e7598dacfd57a9b9
jzetko <jzetkor@gmail.com>
2020-10-13 08:55:41 +0300

    Вторая цифра

--- Циклы for и while/Цикл while  обработка цифр числа/Programming (4)/task.py
+++ Циклы for и while/Цикл while  обработка цифр числа/Programming (4)/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+# Дано натуральное число n(n>9). Напишите программу, которая определяет его вторую (с начала) цифру.
+
+def two_number(n):
+    # a = []
+    # while n > 0:
+    #     a.insert(0, n % 10)
+    #     n = n // 10
+    # return a[1]
+    return n[1]
+
+
+n = (input())
+print(two_number(n))
