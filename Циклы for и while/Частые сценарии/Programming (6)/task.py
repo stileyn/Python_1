51dc91fa7790940996a8ac76963f0214945d95fb
jzetko <jzetkor@gmail.com>
2020-10-13 03:50:09 +0300

    Сумма делителей

--- Циклы for и while/Частые сценарии/Programming (6)/task.py
+++ Циклы for и while/Частые сценарии/Programming (6)/task.py
@@ -1,1 +1,10 @@
-# put your python code here
+# На вход программе подается натуральное число n. Напишите программу, которая вычисляет сумму всех его делителей.
+
+n = int(input())
+summa = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        summa += i
+
+print(summa)
