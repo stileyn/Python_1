7a4612608e56149e194cf6821197b748b694e86b
jzetko <jzetkor@gmail.com>
2020-10-13 03:01:55 +0300

    Последовательность чисел 2

--- Циклы for и while/Цикл for  функция range/Programming (1)/task.py
+++ Циклы for и while/Цикл for  функция range/Programming (1)/task.py
@@ -1,1 +1,11 @@
-# put your python code here
+# Даны два целых числа m и n. Напишите программу, которая выводит все числа от m до n включительно в
+# порядке возрастания, если m<n, или в порядке убывания в противном случае.
+
+m, n = int(input()), int(input())
+
+if m < n:
+    for i in range(m, n + 1):
+        print(i)
+else:
+    for i in range(m, n - 1, -1):
+        print(i)
