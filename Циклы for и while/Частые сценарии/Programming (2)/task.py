3b95f634abd542cac19d4494ce31bbc0fa205376
jzetko <jzetkor@gmail.com>
2020-10-13 03:33:11 +0300

    Асимптотическое приближение

--- Циклы for и while/Частые сценарии/Programming (2)/task.py
+++ Циклы for и while/Частые сценарии/Programming (2)/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+# На вход программе подается натуральное число n. Напишите программу, которая вычисляет значение выражения
+# (1+1/2+1/3+…+1/n)−ln(n).
+
+from math import log
+
+n = int(input())
+n1 = 0
+
+for i in range(1, n + 1):
+    n1 += 1 / i
+
+print(n1 - log(n))
+
