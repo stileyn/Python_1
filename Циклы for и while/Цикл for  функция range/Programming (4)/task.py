a019c361c35ae26ccd66fb17ecc4b56f666e5eb6
jzetko <jzetkor@gmail.com>
2020-10-13 03:13:47 +0300

    Таблица умножения

--- Циклы for и while/Цикл for  функция range/Programming (4)/task.py
+++ Циклы for и while/Цикл for  функция range/Programming (4)/task.py
@@ -1,1 +1,6 @@
-# put your python code here
+# Дано натуральное число n. Напишите программу, которая выводит таблицу умножения на n.
+
+n = int(input())
+
+for i in range(1, 11):
+    print(n, 'x', i, '=', n * i)
