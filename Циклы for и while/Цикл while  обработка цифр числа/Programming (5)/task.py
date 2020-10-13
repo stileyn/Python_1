f3d32feab890b575debbc28d47fca9e773c89970
jzetko <jzetkor@gmail.com>
2020-10-13 09:04:50 +0300

    Одинаковые цифры

--- Циклы for и while/Цикл while  обработка цифр числа/Programming (5)/task.py
+++ Циклы for и while/Цикл while  обработка цифр числа/Programming (5)/task.py
@@ -1,1 +1,7 @@
-# put your python code here
+# Дано натуральное число. Напишите программу, которая определяет, состоит ли указанное число из одинаковых цифр.
+
+def all_the_same(elements):
+    return len(elements) < 1 or len(elements) == elements.count(elements[0])
+
+elemetns = input()
+print('YES' if all_the_same(elemetns) == True else 'NO')
