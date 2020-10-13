fe5c3543987a8159cee4ec03111763001b53d2a1
jzetko <jzetkor@gmail.com>
2020-10-13 03:47:29 +0300

    Без нулей

--- Циклы for и while/Частые сценарии/Programming (5)/task.py
+++ Циклы for и while/Частые сценарии/Programming (5)/task.py
@@ -1,1 +1,10 @@
-# put your python code here
+# Напишите программу, которая считывает 10 чисел и выводит произведение отличных от нуля чисел.
+
+multiplication = 1
+
+for i in range(10):
+    n = int(input())
+    if n != 0:
+        multiplication *= n
+
+print(multiplication)
