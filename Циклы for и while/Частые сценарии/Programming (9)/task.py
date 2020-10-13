398d0d239006e3a8b995320187cee235a03c2aae
jzetko <jzetkor@gmail.com>
2020-10-13 04:32:58 +0300

    Only even numbers

--- Циклы for и while/Частые сценарии/Programming (9)/task.py
+++ Циклы for и while/Частые сценарии/Programming (9)/task.py
@@ -1,1 +1,11 @@
-# put your python code here
+# Напишите программу, которая считывает последовательность из 10 целых чисел и
+# определяет является ли каждое из них четным или нет.
+
+count = 0
+
+for i in range(10):
+    n = int(input())
+    if n % 2 != 0:
+        count += 1
+
+print('NO' if count != 0 else 'YES')
