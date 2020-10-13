a356da2891a679bfa2bbb59d61afc50179bda800
jzetko <jzetkor@gmail.com>
2020-10-13 04:52:20 +0300

    Последовательность Фибоначчи

--- Циклы for и while/Частые сценарии/Programming (10)/task.py
+++ Циклы for и while/Частые сценарии/Programming (10)/task.py
@@ -1,1 +1,17 @@
-# put your python code here
+# Напишите программу, которая считывает натуральное число n и выводит первые n чисел последовательности Фибоначчи.
+
+fib1 = fib2 = 1
+
+n = int(input())
+
+if n < 2:
+    print(fib1, end=' ')
+    quit()
+
+print(fib1, end=' ')
+print(fib2, end=' ')
+
+for i in range(2, n):
+    fib1, fib2 = fib2, fib1 + fib2
+    print(fib2, end=' ')
+
