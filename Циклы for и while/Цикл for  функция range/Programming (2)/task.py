69a5d591a5ce0424c9a2568cf485485321926b56
jzetko <jzetkor@gmail.com>
2020-10-13 03:06:15 +0300

    Последовательность чисел 3

--- Циклы for и while/Цикл for  функция range/Programming (2)/task.py
+++ Циклы for и while/Цикл for  функция range/Programming (2)/task.py
@@ -1,1 +1,11 @@
-# put your python code here
+# Даны два целых числа m и n (m>n). Напишите программу,
+# которая выводит все нечетные числа от m до n включительно в порядке убывания.
+
+m, n = int(input()), int(input())
+
+if m % 2 == 0:
+    for i in range(m - 1, n - 1, -2):
+        print(i)
+else:
+    for i in range(m, n - 1, -2):
+        print(i)
