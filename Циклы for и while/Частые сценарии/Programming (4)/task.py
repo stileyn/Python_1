dad0d0a4b90706cc5a2eb155437488b85e948c98
jzetko <jzetkor@gmail.com>
2020-10-13 03:38:43 +0300

    Факториал

--- Циклы for и while/Частые сценарии/Programming (4)/task.py
+++ Циклы for и while/Частые сценарии/Programming (4)/task.py
@@ -1,1 +1,9 @@
-# put your python code here
+# На вход программе подается натуральное число n. Напишите программу, которая вычисляет n!.
+
+n = int(input())
+factorial = 1
+
+for i in range(1, n + 1):
+    factorial *= i
+
+print(factorial)
