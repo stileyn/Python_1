a8f160c7e982a8156cc5b84de8e427bbe98a83e4
jzetko <jzetkor@gmail.com>
2020-10-14 16:35:21 +0300

    Ревью кода-1

--- Циклы for и while/Поиск ошибок и ревью кода/Programming/task.py
+++ Циклы for и while/Поиск ошибок и ревью кода/Programming/task.py
@@ -1,12 +1,12 @@
 count = 0
-p = 0
-for i in range(1, 10):
+p = 1
+for i in range(1, 11):
     x = int(input())
-    if x > 0:
+    if x >= 0:
         p = p * x
         count = count + 1
 if count > 0:
-    print(x)
+    print(count)
     print(p)
 else:
     print('NO')
