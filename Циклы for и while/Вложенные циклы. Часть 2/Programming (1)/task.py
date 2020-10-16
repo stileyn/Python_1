5d81fe1c9aa7494c1297dfa52c330c813207fa29
jzetko <jzetkor@gmail.com>
2020-10-16 13:00:43 +0300

    Численный треугольник 4

--- Циклы for и while/Вложенные циклы. Часть 2/Programming (1)/task.py
+++ Циклы for и while/Вложенные циклы. Часть 2/Programming (1)/task.py
@@ -1,1 +1,21 @@
-# put your python code here
+# Дано натуральное число n. Напишите программу, которая печатает численный треугольник с высотой равной n,
+# в соответствии с примером:
+
+# 1
+# 121
+# 12321
+# 1234321
+# 123454321
+
+def triangle():
+    n = int(input())
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end='')
+            if j == i:
+                for k in range(i - 1, 0, -1):
+                    print(k, end='')
+        print()
+
+
+triangle()
