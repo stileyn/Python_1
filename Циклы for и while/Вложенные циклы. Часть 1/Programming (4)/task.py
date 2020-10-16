ba13033f0df6783e04326b3415c2f322299bb2f1
jzetko <jzetkor@gmail.com>
2020-10-16 10:08:08 +0300

    Численный треугольник

--- Циклы for и while/Вложенные циклы. Часть 1/Programming (4)/task.py
+++ Циклы for и while/Вложенные циклы. Часть 1/Programming (4)/task.py
@@ -1,1 +1,8 @@
-# put your python code here
+# Дано натуральное число n. Напишите программу, которая печатает численный треугольник
+
+n = int(input())
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(i, end='')
+    print()
