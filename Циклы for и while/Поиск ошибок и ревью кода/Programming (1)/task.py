495a3563e4e1919eaffdb1b3c4d1a50f9f244138
jzetko <jzetkor@gmail.com>
2020-10-16 06:06:53 +0300

    Ревью кода-2

--- Циклы for и while/Поиск ошибок и ревью кода/Programming (1)/task.py
+++ Циклы for и while/Поиск ошибок и ревью кода/Programming (1)/task.py
@@ -1,10 +1,12 @@
-mx = 0
 s = 0
-for i in range(11):
+a = []
+for i in range(10):
     x = int(input())
     if x < 0:
-        s = x
-    if x > mx:
-        mx = x
-print(s)
-print(mx)
+        s += x
+        a.append(x)
+if s == 0:
+    print('NO')
+else:
+    print(s)
+    print(max(a))
