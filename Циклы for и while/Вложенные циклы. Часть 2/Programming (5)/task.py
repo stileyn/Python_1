c30cc1a9e7fb314f4d801021fa7a271c03ee784e
jzetko <jzetkor@gmail.com>
2020-10-17 10:20:03 +0300

    Сумма факториалов

--- Циклы for и while/Вложенные циклы. Часть 2/Programming (5)/task.py
+++ Циклы for и while/Вложенные циклы. Часть 2/Programming (5)/task.py
@@ -1,1 +1,11 @@
-# put your python code here
+# Дано натуральное число n. Напишите программу, которая выводит значение суммы 1!+2!+3!+…+n!.
+from math import factorial
+
+n = int(input())
+summa = 0
+
+for i in range(1, n + 1):
+    fact = factorial(i)
+    summa += fact
+
+print(summa)
