4cad473d3f1894e0f66338e6443013dad57363ee
jzetko <jzetkor@gmail.com>
2020-10-17 13:47:21 +0300

    ФИО

--- Строковый тип данных/Индексация/Programming (4)/task.py
+++ Строковый тип данных/Индексация/Programming (4)/task.py
@@ -1,1 +1,6 @@
-# put your python code here
+# На вход программе подаются три строки: имя, фамилия и отчество.
+# Напишите программу, которая выводит инициалы человека.
+
+surname, name, lastname = [input() for i in range(3)]
+
+print(name[0], surname[0], lastname[0], sep='')
