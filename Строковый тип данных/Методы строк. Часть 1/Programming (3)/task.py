de151dd9803cc86a9f45fd9f6847de9e0b5e5527
jzetko <jzetkor@gmail.com>
2020-10-25 05:36:48 +0300

    Нижний регистр

--- Строковый тип данных/Методы строк. Часть 1/Programming (3)/task.py
+++ Строковый тип данных/Методы строк. Часть 1/Programming (3)/task.py
@@ -1,1 +1,9 @@
-# put your python code here
+# На вход программе подается строка. Напишите программу, которая подсчитывает количество
+# буквенных символов в нижнем регистре.
+
+s = input()
+count = 0
+for i in range(len(s)):
+    if s[i] != s[i].upper():
+        count += 1
+print(count)
