4427ac765df6438caea9a894f7b94edce508d48a
jzetko <jzetkor@gmail.com>
2020-10-25 07:14:41 +0300

    Самый частотный символ

--- Строковый тип данных/Методы строк. Часть 2/Programming (5)/task.py
+++ Строковый тип данных/Методы строк. Часть 2/Programming (5)/task.py
@@ -1,1 +1,10 @@
-# put your python code here
+# На вход программе подается строка текста. Напишите программу,
+# которая выводит на экран символ, который появляется наиболее часто.
+
+string = input()
+maximum, char = 0, ''
+for i in string:
+    if string.count(i) >= maximum:
+        maximum = string.count(i)
+        char = i
+print(char)
