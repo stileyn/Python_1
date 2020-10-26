1e8f49ca3fbf326e9b1cd23cc42cc1482263958d
jzetko <jzetkor@gmail.com>
2020-10-26 23:52:53 +0300

    Удаление фрагмента

--- Строковый тип данных/Методы строк. Часть 3/Programming/task.py
+++ Строковый тип данных/Методы строк. Часть 3/Programming/task.py
@@ -1,3 +1,3 @@
-q, w, e = '2010', '10', 'k'
-s = 'In {0}, someone paid {1}{2} Bitcoin for two pizzas.'.format(q, w, e)
-print(s)
+s = 'In {0}, someone paid {1} {2} for two pizzas.'
+
+print(s.format("2010", "10k", "Bitcoin"))
