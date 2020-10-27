c4c621e21868777d7e37533e3ce664f13cb8bb04
jzetko <jzetkor@gmail.com>
2020-10-27 07:01:22 +0300

    Remove outliers

--- Списки/Вывод элементов списка/Programming (2)/task.py
+++ Списки/Вывод элементов списка/Programming (2)/task.py
@@ -1,1 +1,14 @@
-# put your python code here
+# При анализе данных, собранных в рамках научного эксперимента,
+# бывает полезно удалить самое большое и самое маленькое значение.
+
+# num = [int(input()) for _ in range(int(input()))]
+# num.remove(min(num))
+# num.remove(max(num))
+# print(* num, sep='\n')
+
+l = []
+for i in range(int(input())):
+    l.append(int(input()))
+for a in l:
+    if a != min(l) and a != max(l):
+        print(a)
