4a916ef1fe75256c33f204b27e0a7551b4d75079
jzetko <jzetkor@gmail.com>
2020-10-27 03:56:53 +0300

    Алфавит

--- Списки/Методы списков. Часть 1/Programming (2)/task.py
+++ Списки/Методы списков. Часть 1/Programming (2)/task.py
@@ -1,1 +1,6 @@
-# put your python code here
+# Напишите программу, выводящую следующий список:
+# ['a', 'bb', 'ccc', 'dddd', 'eeeee', 'ffffff', ...]
+l = []
+for i in range(97, 123):
+    l.append(chr(i) * (i - 96))
+print(l)
