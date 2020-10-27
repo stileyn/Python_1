a40b07c51f7384e8f82c3d2913c5672b714a5a41
jzetko <jzetkor@gmail.com>
2020-10-27 05:36:10 +0300

    Удалите нечетные индексы

--- Списки/Методы списков. Часть 1/Programming (7)/task.py
+++ Списки/Методы списков. Часть 1/Programming (7)/task.py
@@ -1,1 +1,16 @@
-# put your python code here
+# На вход программе подается натуральное число n и n строк, а затем число k. Напишите программу,
+# которая выводит k-ую букву из введенных строк на одной строке без пробелов.
+
+n = int(input())
+a = []
+b = ''
+for _ in range(n):
+    n = input()
+    a.append(n)
+
+k = int(input())
+
+for i in range(len(a)):
+    if len(a[i]) >= k:
+        b += a[i][k - 1]
+print(b)
