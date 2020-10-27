26684d12fca73a0e4875244d2067000a9b9779a8
jzetko <jzetkor@gmail.com>
2020-10-27 06:13:16 +0300

    Значение функции

--- Списки/Вывод элементов списка/Programming (1)/task.py
+++ Списки/Вывод элементов списка/Programming (1)/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+# На вход программе подается натуральное число n, а затем n целых чисел. Напишите программу, которая
+# для каждого из введенного числа x выводит значение функции f(x)=x2+2x+1, каждое на отдельной строке.
+
+# n = int(input())
+# b = []
+# for _ in range(n):
+#     n = int(input())
+#     b.append(n)
+#
+# print(* b, '', *[x + 1 ** 2 for x in b], sep='\n')
+
+numbers = [int(input()) for _ in range(int(input()))]
+print(*numbers, '',*[(x + 1) ** 2 for x in numbers], sep='\n')
