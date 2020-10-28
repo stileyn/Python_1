90117c459404d8170f66d42ecc2147dd7cc994ad
jzetko <jzetkor@gmail.com>
2020-10-29 01:34:10 +0300

    Переставить min и max

--- Списки/Методы списков. Часть 2/Programming (1)/task.py
+++ Списки/Методы списков. Часть 2/Programming (1)/task.py
@@ -1,1 +1,10 @@
-# put your python code here
+# На вход программе подается строка текста, содержащая различные натуральные числа. Из данной строки формируется
+# список чисел. Напишите программу, которая меняет местами минимальный и максимальный элемент этого списка.
+
+s = input()
+user_list = [int(number) for number in s.split()]
+
+a, b = user_list.index(min(user_list)), user_list.index(max(user_list))
+user_list[a], user_list[b] = user_list[b], user_list[a]
+print(*user_list)
+
